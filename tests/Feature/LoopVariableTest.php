6d601ad71e4ee70b9ae878f0a9a8457a959d9af1
k1adi <rizkiadinugroho3@gmail.com>
2024-05-31 16:42:32 +0700

    implementation loop property part of looping method

---
+++ tests/Feature/LoopVariableTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoopVariableTest extends TestCase
+{
+    public function testLoopVariable()
+    {
+        $this->view('loop-variable', [
+            'skills' => ['Coding', 'Riding']
+        ])
+        ->assertSeeText('1. Coding')
+        ->assertSeeText('2. Riding');
+    }
+}

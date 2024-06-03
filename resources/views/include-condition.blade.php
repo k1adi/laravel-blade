d68349cb96da6e68851978d51c2c11b4c0658ed5
k1adi <rizkiadinugroho3@gmail.com>
2024-06-03 17:24:04 +0700

    minor changes at implementation include laravel blade tempalte

--- resources/views/include-condition.blade.php
+++ resources/views/include-condition.blade.php
@@ -4,13 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Include Blade</title>
+    <title>Include Condition</title>
 </head>
 <body>
-    @include('layout.header', [
-        'description' => 'Lorem ipsum dolor sit amet'
-    ]);
-
-    <p>Selamat Datang!</p>
+    
 </body>
 </html>

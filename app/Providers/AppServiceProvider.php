3b183eae5fd7ecf760a832f5a4c537588493818a
k1adi <rizkiadinugroho3@gmail.com>
2024-06-04 13:37:46 +0700

    implementation service injection blade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\SayHello;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(SayHello::class, function() {
+            return new SayHello();
+        });
     }
 
     /**
@@ -23,6 +27,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Blade::directive("hello", function($expression) {
+            return "<?php echo 'Hello ' . $expression; ?>";
+        });
     }
 }
---
+++ tests/Feature/ServiceInjectionTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ServiceInjectionTest extends TestCase
+{
+    public function testServiceInjection()
+    {
+        $this->view('service-injection', [
+            'name' => 'Rizki'
+        ])
+        ->assertSeeText('Hello Rizki!');
+    }
+}

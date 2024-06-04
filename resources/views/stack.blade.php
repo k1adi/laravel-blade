ae8ddf2f7944389622975836480cee7013884656
k1adi <rizkiadinugroho3@gmail.com>
2024-06-04 11:13:28 +0700

    implementation stack in blade template

---
+++ resources/views/stack.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Stack Blade</title>
+</head>
+<body>
+    @push('script')
+        <script src="first.js"></script>
+    @endpush
+    
+    @push('script')
+        <script src="second.js"></script>
+    @endpush
+    
+    @push('script')
+        <script src="third.js"></script>
+    @endpush
+
+    @prepend('script')
+        <script src='fourth.js'></script>
+    @endprepend
+
+    @stack('script')
+</body>
+</html>
